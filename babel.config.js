6c45b1a5c10ee3c07712ed267a63b1a984009f10
devPandanjo <147170622+devPandanjo@users.noreply.github.com>
2024-06-26 22:02:05 -0300

    run build for 1st time

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,10 @@ module.exports = function(api) {
         "moduleName": "@env",
         "path": ".env",
         "allowUndefined": false
-      }]
+      }],
+      ["@babel/plugin-transform-class-properties", { "loose": true }],
+      ["@babel/plugin-transform-private-methods", { "loose": true }],
+      ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
     ]
   };
 };

60df57ad86b13e19c92e0d5e484aa22a08727f2d
bporato <brunaporato@pandanjo.com>
2024-07-02 07:57:20 -0300

    create text area input component

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Container, Content } from './styles';
 import { DepartureHeader } from '../../components/DepartureHeader';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
+import { TextAreaInput } from '../../components/TextAreaInput';
+import { Button } from '../../components/Button';
 
 export function Departure() {
   return (
@@ -12,6 +14,9 @@ export function Departure() {
           label='License Plate'
           placeholder='BRA-1234'
         />
+        <TextAreaInput label='Purpose' placeholder="I'll use this vehicle for..." />
+
+        <Button title='Register Departure' />
       </Content>
     </Container>
   );

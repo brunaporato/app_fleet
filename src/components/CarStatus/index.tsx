fe88f093bd241bc47cab58d4259db368f23f2a8f
bporato <brunaporato@pandanjo.com>
2024-07-02 07:01:34 -0300

    create car status component + navigation to departure screen

---
+++ src/components/CarStatus/index.tsx
@@ -0,0 +1,31 @@
+import { Car, Key } from 'phosphor-react-native';
+import { Container, IconBox, Message, TextHighlight } from './styles';
+import { useTheme } from 'styled-components';
+import { TouchableOpacityProps } from 'react-native';
+
+type Props = TouchableOpacityProps & {
+  licensePlate?: string
+}
+
+export function CarStatus({ licensePlate, ...rest }: Props) {
+  const theme = useTheme()
+  
+  const Icon = licensePlate ? Key : Car
+  const message = licensePlate ? `Vehicle ${licensePlate} currently in use. ` : 'No vehicle is currently in use. '
+  const status = licensePlate ? 'arrival' : 'departure'
+
+  return (
+    <Container activeOpacity={0.7} {...rest}>
+      <IconBox>
+        <Icon size={32} color={theme.COLORS.BRAND_LIGHT} />
+      </IconBox>
+      <Message>
+        {message}
+
+        <TextHighlight>
+          Press here to register your {status}.
+        </TextHighlight>
+      </Message>
+    </Container>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,20 @@
-import { Container } from './styles';
+import { Container, Content } from './styles';
 import { Header } from '../../components/Header';
+import { CarStatus } from '../../components/CarStatus';
+import { useNavigation } from '@react-navigation/native';
 
 export function Home() {
+  const { navigate } = useNavigation()
+
+  function handleRegisterMovement() {
+    navigate('departure')
+  }
   return (
     <Container>
       <Header />
+      <Content>
+        <CarStatus onPress={handleRegisterMovement} />
+      </Content>
     </Container>
   );
 }

b599a1a865e4ad007a7f72d170278c4e0ec1d4e0
bporato <brunaporato@pandanjo.com>
2024-07-02 07:48:10 -0300

    create license plate input component

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Content } from './styles';
 import { DepartureHeader } from '../../components/DepartureHeader';
+import { LicensePlateInput } from '../../components/LicensePlateInput';
 
 export function Departure() {
   return (
     <Container>
       <DepartureHeader title='Departure' />
+      <Content>
+        <LicensePlateInput
+          label='License Plate'
+          placeholder='BRA-1234'
+        />
+      </Content>
     </Container>
   );
 }

445160200d6d1d8cbb102ad723ad6d1a98b6bfde
bporato <brunaporato@pandanjo.com>
2024-07-05 07:33:07 -0300

    canceling registration

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,12 +1,13 @@
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
 import { DepartureHeader } from '../../components/DepartureHeader';
 import { Button } from '../../components/Button';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { X } from 'phosphor-react-native';
-import { useObject } from '../../libs/realm';
+import { useObject, useRealm } from '../../libs/realm';
 import { History } from '../../libs/realm/schemas/History';
 import { BSON } from 'realm';
+import { Alert } from 'react-native';
 
 type RouteParamsProps = {
   id: string
@@ -17,6 +18,27 @@ export function Arrival() {
   const { id } = route.params as RouteParamsProps
 
   const history = useObject(History, new BSON.UUID(id) as unknown as string)
+  const realm = useRealm()
+  const navigation = useNavigation()
+
+  function handleCancelRegistration() {
+    Alert.alert(
+      'Cancel',
+      'Cancel the registration of this vehicle?',
+      [
+        { text: 'No', style: 'cancel' },
+        { text: 'Yes', onPress: () => cancelRegistration() }
+      ]
+    )
+  }
+
+  function cancelRegistration() {
+    realm.write(() => {
+      realm.delete(history)
+    })
+
+    navigation.goBack()
+  }
 
   return (
     <Container>
@@ -33,7 +55,7 @@ export function Arrival() {
         </Description>
 
         <Footer>
-          <ButtonIcon icon={X} />
+          <ButtonIcon icon={X} onPress={handleCancelRegistration} />
           <Button title='Register Arrival' />
         </Footer>
 
